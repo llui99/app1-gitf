2c2d72d9f4baa8314b985faab3f163f244e1f50b
FherCopal <38224705+FherCopal@users.noreply.github.com>
2018-04-11 09:30:45 -0500

    subida3

--- js/acciones.js
+++ js/acciones.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 
- $(document).ready(function (e)){
+ $(document).ready(function (e){
 	 document.addEventListener("deviceready",function(){
 		 $('#disp table td').eq(3).text(device.model);
 		 $('#disp table td').eq(5).text(device.cordova);
@@ -21,13 +21,13 @@
 		 },false);//se desconecto
 		 
 		 
-	 },false;//ready device
+	 },false);//ready device
 	 
 	 
  });//document
  
  function escribehistoria(accion){
-	 $('#eHistoria').append('<li>'+accion+'</li>'>);
+	 $('#eHistoria').append('<li>'+accion+'</li>');
  }
  
 	 
